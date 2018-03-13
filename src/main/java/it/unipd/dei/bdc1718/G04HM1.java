cd63f730b1edc1cb07b186316250af4712075994
gendarius <dario.fac@gmail.com>
2018-03-13 18:55:11 +0100

    Added first part of point 3. Probably (with probability >= 1 - delta over the choice of map/reduce) not the way it is supposed to be done.

--- src/main/java/it/unipd/dei/bdc1718/G04HM1.java
+++ src/main/java/it/unipd/dei/bdc1718/G04HM1.java
@@ -6,7 +6,9 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class G04HM1 {
@@ -19,31 +21,32 @@ public class G04HM1 {
 
         // Read a list of numbers from the program options
         ArrayList<Double> lNumbers = new ArrayList<>();
-        Scanner s =  new Scanner(new File(args[0]));
-        while (s.hasNext()){
+        Scanner s = new Scanner(new File(args[0]));
+        while (s.hasNext()) {
             lNumbers.add(Double.parseDouble(s.next()));
         }
         s.close();
 
         // Setup Spark
-        SparkConf conf = new SparkConf(true)
-                .setAppName("Preliminaries");
+        SparkConf conf = new SparkConf(true).setAppName("FirstHomework");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         // Create a parallel collection
         JavaRDD<Double> dNumbers = sc.parallelize(lNumbers);
 
-
         // POINT 2
+        // In the variable mean I store the sum of the mean of the elements of the data set
+        double mean = dNumbers.reduce((x, y) -> x + y) / dNumbers.count();
+        System.out.println("Average: " + mean);
 
-        //In the variable sum I store the sum of the mean of the elements of the data set
-        double mean = dNumbers.reduce((x, y) -> x + y)/dNumbers.count();
-        System.out.println("Average: "+mean);
-
-        JavaRDD<Double> dDiffavgs = dNumbers.map((x) -> Math.abs(mean-x));
+        JavaRDD<Double> dDiffavgs = dNumbers.map((x) -> Math.abs(mean - x));
         dDiffavgs.foreach((x) -> System.out.println(x));
 
-
+        // POINT 3
+        // Sorting elements by their value
+        dDiffavgs = dDiffavgs.sortBy(x -> x.doubleValue(), true, 1);
+        double min = dDiffavgs.first();
+        System.out.println("The minimum distance from the mean is: " + min);
     }
 
 }
