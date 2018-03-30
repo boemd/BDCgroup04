00b493fc546abf9dd2761016cdd2072e5fe9cabd
nicolaboscaro <nicola@klee.eu>
2018-03-30 18:38:35 +0200

    Start of HM2 (Aggiunto il codice scritto da Dario)

--- src/main/java/it/unipd/dei/bdc1718/G04HM2.java
+++ src/main/java/it/unipd/dei/bdc1718/G04HM2.java
@@ -35,5 +35,36 @@ import java.util.*;
 import java.io.*;
 
 public class G04HM2 {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        // ---------------------CODICE DI DARIO--------------------------
+
+        // Parse the path to the data
+        String path = args[0];
+
+        // Initialize Spark
+        Logger.getLogger("org").setLevel(Level.OFF);
+        Logger.getLogger("akka").setLevel(Level.OFF);
+        SparkConf sparkConf = new SparkConf(true).setAppName("Basic Word Count");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        // Read the data from the text file
+        int numPartitions = sc.defaultParallelism();
+        JavaRDD<String> dDocs = sc.textFile(path, numPartitions);
+
+        // Iterator
+        JavaRDD<String> dWords = dDocs.flatMap((doc) -> Arrays.stream(doc.split(" ")).iterator());
+
+        // new JavaPairRDD to store (key, value) pairs
+        // Each word is mapped into a tuple (word, 1)
+        // Then using reduceByKey we gather together all pairs with same key and increase value
+        JavaPairRDD<String, Integer> dCounts = dWords.mapToPair((w) -> new Tuple2<>(w, 1)).reduceByKey((x, y) -> x + y);
+
+        // Swap (key, value) to (value, key) to use sortByKey
+        JavaPairRDD<Integer, String> dSwapped = dCounts.mapToPair((w) -> new Tuple2<>(w._2, w._1));
+        dSwapped = dSwapped.sortByKey(true, 1);
+
+        // Output is word : occurrences
+        dSwapped.foreach( x -> System.out.println(x._2 + " : " + x._1));
+    }
 }
