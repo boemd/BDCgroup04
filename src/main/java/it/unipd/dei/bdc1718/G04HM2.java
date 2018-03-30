0673177395d2c4c7b0b960d8426d708f42e7c15b
nicolaboscaro <nicola@klee.eu>
2018-03-30 15:53:18 +0200

    Start of HM2

---
+++ src/main/java/it/unipd/dei/bdc1718/G04HM2.java
@@ -0,0 +1,15 @@
+package it.unipd.dei.bdc1718;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import scala.Serializable;
+import java.util.*;
+import java.io.*;
+
+public class G04HM2 {
+}
