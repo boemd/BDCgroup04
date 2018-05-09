374be9c087f79e5287425eee70315a17c3630352
gendarius <dario.fac@gmail.com>
2018-05-09 18:31:09 +0200

    Read vectors.

---
+++ src/main/java/it/unipd/dei/bdc1718/G04HM4.java
@@ -0,0 +1,35 @@
+package it.unipd.dei.bdc1718;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class G04HM4 {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Expecting the file name on the command line");
+        }
+        String path = args[0];
+
+        Logger.getLogger("org").setLevel(Level.OFF);
+        Logger.getLogger("akka").setLevel(Level.OFF);
+
+        SparkConf conf = new SparkConf(true).setAppName("Fourth Homework");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Load a text file into an RDD of strings, where each string corresponds to a distinct line (document) of the file
+        int numPartitions = sc.defaultParallelism();
+
+        // Create JavaRDD from input path
+        JavaRDD<Vector> pointsrdd = InputOutput.readVectors(sc,path);
+    }
+}
