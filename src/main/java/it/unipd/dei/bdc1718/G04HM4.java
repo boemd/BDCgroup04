ea79646ea4950c452375383755b27cbd32dc31c4
nicolaboscaro <nicola@klee.eu>
2018-05-14 11:06:22 +0200

    Aggiunto il metodo per fare il punto 1 fornito dal prof

--- src/main/java/it/unipd/dei/bdc1718/G04HM4.java
+++ src/main/java/it/unipd/dei/bdc1718/G04HM4.java
@@ -11,6 +11,7 @@ import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ListIterator;
 import java.util.Scanner;
 
@@ -46,7 +47,63 @@ public class G04HM4 {
         }
         return sum/(numPoints*(numPoints-1)/2);
     }
+
+
+    /**
+     * Sequential approximation algorithm based on matching provided by the link in the assignment.
+     */
+
+    public static ArrayList<Vector> runSequential(final ArrayList<Vector> points, int k) {
+        final int n = points.size();
+        if (k >= n) {
+            return points;
+        }
+
+        ArrayList<Vector> result = new ArrayList<>(k);
+        boolean[] candidates = new boolean[n];
+        Arrays.fill(candidates, true);
+        for (int iter=0; iter<k/2; iter++) {
+            // Find the maximum distance pair among the candidates
+            double maxDist = 0;
+            int maxI = 0;
+            int maxJ = 0;
+            for (int i = 0; i < n; i++) {
+                if (candidates[i]) {
+                    for (int j = i+1; j < n; j++) {
+                        if (candidates[j]) {
+                            double d = Math.sqrt(Vectors.sqdist(points.get(i), points.get(j)));
+                            if (d > maxDist) {
+                                maxDist = d;
+                                maxI = i;
+                                maxJ = j;
+                            }
+                        }
+                    }
+                }
+            }
+            // Add the points maximizing the distance to the solution
+            result.add(points.get(maxI));
+            result.add(points.get(maxJ));
+            // Remove them from the set of candidates
+            candidates[maxI] = false;
+            candidates[maxJ] = false;
+        }
+        // Add an arbitrary point to the solution, if k is odd.
+        if (k % 2 != 0) {
+            for (int i = 0; i < n; i++) {
+                if (candidates[i]) {
+                    result.add(points.get(i));
+                    break;
+                }
+            }
+        }
+        if (result.size() != k) {
+            throw new IllegalStateException("Result of the wrong size");
+        }
+        return result;
+    }
 }
+
 
 
 
