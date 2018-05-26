5f073007b5f990549207a330c9deedfd27904402
gendarius <35623244+gendarius@users.noreply.github.com>
2018-05-26 11:50:36 +0200

    Fixed something

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for k in {10..200..10}
 	do 
-		echo $k : >> output.txt
+		echo Results : >> output$k.txt
 		spark-submit —total-executor-cores 72 —executor-cores 8 —class it.unipd.dei.bdc1718.G04HM4 BigData-all.jar vecs-50-10000.txt $k >> output$k.txt
 	done
